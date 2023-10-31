a306aa31fca8f25eb1990bf41d02e8b0e151378d
thiagosartore07 <133020073+thiagosartore07@users.noreply.github.com>
2023-10-31 11:34:27 -0300

    Add files via upload

---
+++ GDscript examen/ej3.js
@@ -0,0 +1,13 @@
+extends Node
+var inventory = { 
+"name": "Julian", 
+    
+"surname": "Alvarez", 
+    
+"goals": 4, 
+    
+"teams": ["Polonia" "Australia", "Croacia", "Croacia"]
+
+func messi(jualian,alvarez):
+     
+  pass
---
+++ GDscript examen/ej4.js
@@ -0,0 +1,10 @@
+extends Node
+var x = [1,3,5]
+var f = []
+
+func messi(x):
+     if (x * 2 -= 1 *2)
+        f.push(x)
+        x = f
+  pass
+console.log(messi([f]));
